8e18204044ddd8eae2d5c8889a98bb991832db83
panicinthe <irina.mart.r@gmail.com>
2020-11-17 16:05:09 +0300

    create downloader csv file and line break

---
+++ download.py
@@ -0,0 +1,16 @@
+from urllib import request
+
+goog_url = "https://people.sc.fsu.edu/~jburkardt/data/csv/addresses.csv"
+def download(csv_url):
+    response = request.urlopen(csv_url)
+    csv = response.read()
+    csv_str = str(csv)
+    lines = csv_str.split("\\n")
+    dest_url = r"goog.csv"
+    fx = open(dest_url, "w")
+    for line in lines:
+        fx.write(line + "\\n")
+    fx.close()
+
+
+download(goog_url)
